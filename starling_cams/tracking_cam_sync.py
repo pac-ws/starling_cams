f5db8bb38a926688e4c8ba7265c40e93fe9fabdb
fred <fred.vatnsdal@gmail.com>
2025-11-04 11:32:27 -0500

    Adjust parameters

--- starling_cams/tracking_cam_sync.py
+++ starling_cams/tracking_cam_sync.py
@@ -13,9 +13,9 @@ class TrackingCamSync(Node):
         super().__init__("tracking_cam_sync")
 
         # Parameters
-        self.declare_parameter("debug", False)
+        self.declare_parameter("debug", rclpy.Parameter.Type.BOOL)
         self.debug = self.get_parameter("debug").get_parameter_value().bool_value
-        self.declare_parameter("spam_rate", 125) # logging in callback every 5 seconds
+        self.declare_parameter("spam_rate", rclpy.Parameter.Type.INT) # logging in callback every 5 seconds
         self.spam_rate = self.get_parameter("spam_rate").get_parameter_value().int_value
         self.get_logger().info(f"Debug mode: {self.debug}")
         self.get_logger().info(f"Spam rate: {self.spam_rate}")
